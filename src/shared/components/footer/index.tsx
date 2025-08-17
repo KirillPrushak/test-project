16b2a9ecda9cc42ba0a040a3ce9f1a48dce3671a
KIrillPrushak <kirillprushak@gmail.com>
2025-08-17 23:40:43 +0300

    feat: add footer

--- src/shared/components/footer/index.tsx
+++ src/shared/components/footer/index.tsx
@@ -1,5 +1,23 @@
+import "./index.scss";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="wrapper">
+        <div className="description-data">
+          <p>OwlTop © 2020 - 2021 Все права защищены</p>
+        </div>
+        <div className="description-rules">
+          <div className="user">
+            <p>Пользовательское соглашение</p>
+          </div>
+          <div className="policy">
+            <p>Политика конфиденциальности</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
