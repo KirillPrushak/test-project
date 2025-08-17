432f2abb3dc406e46f084adb5ab84060fb5f59e1
KIrillPrushak <kirillprushak@gmail.com>
2025-08-18 02:00:24 +0300

    feat: add button components

---
+++ src/shared/components/buttons/buttonCourses/index.tsx
@@ -0,0 +1,11 @@
+import "./index.scss";
+
+function ButtonCourses() {
+  return (
+    <a href="/data" className="btn-courses">
+      Выбрать курсы
+    </a>
+  );
+}
+
+export default ButtonCourses;
