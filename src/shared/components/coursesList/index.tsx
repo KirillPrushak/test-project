d78c77eed8e753e40fb2f5ffab70cb7114f2ad88
KIrillPrushak <kirillprushak@gmail.com>
2025-08-18 14:26:15 +0300

    feat: add list of courses

---
+++ src/shared/components/coursesList/index.tsx
@@ -0,0 +1,19 @@
+import Card from "../card";
+import "./index.scss";
+
+function CoursesList() {
+  return (
+    <div className="list">
+      <div className="container">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+}
+
+export default CoursesList;
