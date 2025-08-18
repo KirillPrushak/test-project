8e79d95542cd89c2394de8e9b3c67907f530032b
KIrillPrushak <kirillprushak@gmail.com>
2025-08-18 04:25:41 +0300

    feat: add card

---
+++ src/shared/components/card/index.tsx
@@ -0,0 +1,14 @@
+import "./index.scss";
+
+function Card() {
+  return (
+    <div className="card">
+      {/* <img src="" alt="" /> */}
+      <h3>Название курса</h3>
+      <p>Описание курса</p>
+      <button>Учиться</button>
+    </div>
+  );
+}
+
+export default Card;
