148e8f33f036ace028244a1f46924894d7926553
KIrillPrushak <kirillprushak@gmail.com>
2025-08-18 04:24:36 +0300

    feat: add sidebar

--- src/shared/sidebar/index.tsx
+++ src/shared/sidebar/index.tsx
@@ -1,5 +1,39 @@
+import ButtonAdd from "../components/buttons/buttonAdd";
+import ButtonDelete from "../components/buttons/buttonDelete";
+import "./index.scss";
+
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>
+          <span>Photoshop</span>
+          <ButtonAdd />
+          <ButtonDelete />
+        </li>
+        <li>
+          <span>AfterEffect</span>
+          <ButtonAdd />
+          <ButtonDelete />
+        </li>
+        <li>
+          <span>Frontend</span>
+          <ButtonAdd />
+          <ButtonDelete />
+        </li>
+        <li>
+          <span>Backend</span>
+          <ButtonAdd />
+          <ButtonDelete />
+        </li>
+        <li>
+          <span>Gamedev</span>
+          <ButtonAdd />
+          <ButtonDelete />
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
