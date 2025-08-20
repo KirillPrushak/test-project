67a33361bedb4a8d3abc4dc17ee48170b9bdbdcd
KIrillPrushak <kirillprushak@gmail.com>
2025-08-20 18:43:09 +0300

    feat: add the ability to change the duration

--- src/shared/components/card/index.tsx
+++ src/shared/components/card/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 import "./index.scss";
 import ButtonSend from "../buttons/ButtonSend";
 export interface ICardProps {
@@ -11,9 +11,26 @@ export interface ICardProps {
 
 function Card({ title, description, instructor, duration, price }: ICardProps) {
   const [isActiveToggle, setIsToggleActive] = useState<boolean>(false);
+  const [switchDuration, setSwitchDuration] = useState<string>(duration);
+  const [switchPrice, setSwitchPrice] = useState<number>(price);
+
+  // const numberMount = switchDuration.match(/\d+/);
+  const numberCurrentMount = parseInt(switchDuration[0]);
+  const currentPriceForOneMount = price / numberCurrentMount;
+  const currentPriceForSelectMount =
+    currentPriceForOneMount * numberCurrentMount;
+
+  console.log(switchDuration[0]);
+  console.log(currentPriceForOneMount);
 
   const handleSwitchDuration = () => {
     setIsToggleActive(!isActiveToggle);
+  };
+
+  const handleChangeDuration = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setSwitchDuration(event.target.value);
   };
 
   return (
@@ -22,15 +39,17 @@ function Card({ title, description, instructor, duration, price }: ICardProps) {
         <h2>{title}</h2>
         <p>{description}</p>
       </div>
-      <p>Преподаватель: {instructor}</p>
+      <p>
+        Преподаватель: <span>{instructor}</span>
+      </p>
       <div className="container-change">
         <p>
           Длительность:
-          <span> {duration}</span>
+          <span className="price"> {numberCurrentMount}</span>
         </p>
         <p>
           Стоимость:
-          <span> {price}</span>
+          <span> {numberCurrentMount * (price / parseInt(duration[0]))}₽</span>
         </p>
       </div>
       {!isActiveToggle ? (
@@ -40,11 +59,13 @@ function Card({ title, description, instructor, duration, price }: ICardProps) {
             alt="editing"
             onClick={handleSwitchDuration}
           />
-          <ButtonSend />
+          {/* <ButtonSend /> */}
         </div>
       ) : (
         <div className="toggle-change">
-          <select>
+          <select value={switchDuration} onChange={handleChangeDuration}>
+            <option value="1 месяц">1 месяц</option>
+            <option value="2 месяца">2 месяца</option>
             <option value="3 месяца">3 месяца</option>
             <option value="4 месяца">4 месяца</option>
             <option value="5 месяцев">5 месяца</option>
